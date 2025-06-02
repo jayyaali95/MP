78ee16d0b47beba1d72ec582f7822d2b7f75fc6a
Javad (Jay) YaAli <javad.yaali@gmail.com>
2025-06-01 20:41:28 -0400

    Update metaclass.py -- add extra

--- tasks/schema_validator/metaclass.py
+++ tasks/schema_validator/metaclass.py
@@ -4,10 +4,12 @@ from utils import Column
 class SchemaMeta(type):
     def __new__(mcs, name, bases, namespace):
         cols = {}
+
         for key, value in list(namespace.items()):
             if isinstance(value, Column):
                 cols[key] = value
         namespace['_columns_'] = cols
+        namespace['_allow_extra_columns'] = namespace.get('_allow_extra_columns', False)
         return super().__new__(mcs, name, bases, namespace)
     
 Schema = SchemaMeta('Schema', (), {})
